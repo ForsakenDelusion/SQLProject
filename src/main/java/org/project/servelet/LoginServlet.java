bc4bbe6cb628a9177c079b56336777ff3b3d4092
ForsakenDelusion <wangcy0205@outlook.com>
2024-05-27 03:23:10 +0800

    Initial commit

---
+++ src/main/java/org/project/servelet/LoginServlet.java
@@ -0,0 +1,20 @@
+package org.project.servelet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.project.utils.ThymeleafUtil;
+import org.thymeleaf.context.Context;
+
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ThymeleafUtil.process("login.html", new Context(), resp.getWriter());
+    }
+}
---
+++ src/main/resources/script.js
@@ -0,0 +1,23 @@
+// 列出所有音频文件的路径
+const audioFiles = [
+    '/static/audio/audio1.MP3',
+    '/static/audio/audio2.MP3',
+    '/static/audio/audio3.MP3',
+    '/static/audio/audio4.MP3',
+    '/static/audio/audio5.MP3',
+    '/static/audio/audio6.MP3',
+    '/static/audio/audio7.MP3',
+    // 添加更多音频文件路径
+];
+
+document.getElementById('Atri').addEventListener('click', function() {
+    const audio = document.getElementById('myAudio');
+
+    // 随机选择一个音频文件
+    const randomIndex = Math.floor(Math.random() * audioFiles.length);
+    const selectedAudio = audioFiles[randomIndex];
+
+    // 设置音频源并播放
+    audio.src = selectedAudio;
+    audio.play();
+});
